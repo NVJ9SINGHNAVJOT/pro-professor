dd4f363aa95b49dc10a8b97fed0aa8dac1bcaae2
Navjot Singh <nvjsinghnavjot4411@gmail.com>
2025-11-09 23:36:10 +0530

    feat: chat page updated

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,7 +1,12 @@
 import Page from "@/components/wrapper/Page";
 
 const Chat = () => {
-  return <Page className="">Chat</Page>;
+  return (
+    <Page className="flex">
+      <section className="text-white w-[270px] bg-grey-50">SideBar</section>
+      <section className="bg-grey flex-1">Chat Messages</section>
+    </Page>
+  );
 };
 
 export default Chat;
